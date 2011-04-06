6c9412bb131970dedd8b270b19553b225db2593d
Kevin Charter <kevin.charter@acm.org>
2011-04-05 22:39:01 -0600

    Add Lev3 and Lev4 to the unit tests.
    I'm surprised that I hadn't done this already. Good thing the
    tests pass. :-)

--- lev-test.hs
+++ lev-test.hs
@@ -3,6 +3,8 @@ module Main where
 import qualified Reference as Ref
 import qualified Lev1 as Lev1
 import qualified Lev2 as Lev2
+import qualified Lev3 as Lev3
+import qualified Lev4 as Lev4
 import qualified Lev5 as Lev5
 import Test
 
@@ -15,6 +17,10 @@ main = do
   test Lev1.lev
   putStrLn "Test the Lev2 implementation"
   test Lev2.lev
+  putStrLn "Test the Lev3 implementation"
+  test Lev3.lev
+  putStrLn "Test the Lev4 implementation"
+  test Lev4.lev
   putStrLn "Test the Lev5 implementation"
   test Lev5.lev
 
